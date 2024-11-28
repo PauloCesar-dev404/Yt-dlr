42a985e50d533a0c9e996d2f1ac048f8e94e76b9
Paulo César <pauloguitarrasoms@gmail.com>
2024-11-27 20:05:05 -0400

    Atualizar o setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,11 +1,11 @@
 import nutikacompile
 
-nutikacompile.compile_with_nuitka(pyfile=r'C:\PycharmProjects\yt-dll\yt-dlr.py',
+nutikacompile.compile_with_nuitka(pyfile=r'./yt-dlr.py',
                                   copyright='PauloCesar0073',
                                   product_name=r'yt-dlr',
                                   outputdir=r'yt-dlr',
                                   onefile=True,
-                                  icon=r'C:\PycharmProjects\yt-dll\favicon.ico',
+                                  icon=r'./favicon.ico',
                                   delete_onefile_temp=True,
                                   disable_console=False,
                                   file_version='1.0.1',
